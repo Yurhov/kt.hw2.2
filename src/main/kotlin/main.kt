e37f94aeaab15d4fc03fa2d67e1009f836d410d9
Yurhov <yurhov@yandex.ru>
2022-03-26 19:39:21 +0300

    1 commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    while (true) {
+        print("\nЧтобы закончить введите \"0\"\nВведите число лайков:  ")
+        val likes = readLine()?.toInt() ?: return
+        if (likes == 0) break
+        val phrase = if (likes == 1 || (likes - 1) % 10 == 0 && likes != 11) "человеку"
+        else "людям"
+        println("Понравилось $likes $phrase")
+        println((likes - 1) % 10)
+    }
+    println("Досвидания")
+}
