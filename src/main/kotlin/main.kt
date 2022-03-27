8572eb88462b4d889c33206db02fd1ac5b80ec11
Yurhov <yurhov@yandex.ru>
2022-03-27 11:10:13 +0300

    Добавление условия для 111

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,9 @@ fun main() {
         print("\nЧтобы закончить введите \"0\"\nВведите число лайков:  ")
         val likes = readLine()?.toInt() ?: return
         if (likes == 0) break
-        val phrase = if (likes == 1 || (likes - 1) % 10 == 0 && likes != 11) "человеку"
+        val phrase = if (likes == 1 || (likes - 1) % 10 == 0 && likes != 11 && (likes - 11) % 100 != 0) "человеку"
         else "людям"
         println("Понравилось $likes $phrase")
-        println((likes - 1) % 10)
     }
     println("Всего хорошего!")
 }
