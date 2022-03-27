471a71cf4b912b0bf32f9a2e1539f6ab36922fb8
Yurhov <yurhov@yandex.ru>
2022-03-27 12:31:57 +0300

    Изменение условий

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
 fun main() {
     while (true) {
         print("\nЧтобы закончить введите \"0\"\nВведите число лайков:  ")
-        val likes = readLine()?.toInt() ?: return
-        if (likes == 0) break
-        val phrase = if (likes == 1 || (likes - 1) % 10 == 0 && likes != 11 && (likes - 11) % 100 != 0) "человеку"
+        val likes = readLine()?.toUInt() ?: return
+        if (likes == 0U) break
+        val phrase = if (likes % 10U == 1U && likes % 100U != 11U) "человеку"
         else "людям"
         println("Понравилось $likes $phrase")
     }
